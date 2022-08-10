f1a033df2a699ca5a905f4c0b847a0aea7efa4eb
segin-GH <segin.bytes@gmail.com>
2022-08-11 00:26:43 +0530

    .

---
+++ CS50P/matchFunction/main.py
@@ -0,0 +1,27 @@
+
+def main():
+
+    name = input("what is your name? ")
+    match name:
+        case "harry" | "hermione" | "ron" | "ginny":
+            print("griffindor")
+        case "draco":
+            print("slytherin")
+        case _:
+            print("who ?")
+
+
+    # match name:
+    #     case "harry":
+    #         print("griffindor")
+    #     case "ron":
+    #         print("griffindor")
+    #     case "hermione":
+    #         print("griffindor")
+    #     case "draco":
+    #         print("slytherin")
+    #     case _:
+    #         print("who ?")
+
+
+main() 
