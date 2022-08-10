5d5550cad349ddb3db2bbdb41290e5cc449d89f5
segin-GH <segin.bytes@gmail.com>
2022-08-11 00:14:03 +0530

    .

---
+++ CS50P/parity/main.py
@@ -0,0 +1,18 @@
+
+
+def main():
+
+    x = int(input("enter a number : "))
+    if is_even(x):
+        print("your number is even.")
+    else:
+        print("your number is odd.")
+
+
+def is_even(n):
+    
+    return n % 2 == 0
+    # return True if n % 2 == 0 else False
+
+
+main()
