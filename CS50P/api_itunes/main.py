d57dd0985824b14495981a3d5908d7a3e9419f36
segin-GH <segin.bytes@gmail.com>
2022-08-17 00:00:02 +0530

    .

---
+++ CS50P/api_itunes/main.py
@@ -0,0 +1,23 @@
+
+
+import requests
+import json
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit()
+
+    repo = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term="+sys.argv[1] )
+
+    # print(json.dumps(repo.json(), indent = 3))
+
+    o = repo.json()
+    for res in o["results"]:
+        print(res["trackName"])
+
+
+
+
+main()
