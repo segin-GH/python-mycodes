356875d0e8c418531a05418f63d8ecaf11f092df
segin-GH <segin.bytes@gmail.com>
2022-08-16 23:11:51 +0530

    .

---
+++ CS50P/system argv/main.py
@@ -0,0 +1,16 @@
+
+import sys
+
+
+def main():
+    
+        try:
+            print("hello,",sys.argv[1])
+        
+        except:
+
+            print("Error ::  Did you write your name ? ")
+
+
+
+main()
