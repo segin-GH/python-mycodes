fc22851012a570bee96389bf19dce8ca6be20201
segin-GH <segin.bytes@gmail.com>
2022-08-25 00:45:46 +0530

    .

--- CS50P/class&obj/main.py
+++ CS50P/class&obj/main.py
@@ -1,36 +1,22 @@
 
 class Student:
 
-    def __init__(self, name, house, pet):
+    def __init__(self, name, house):
         if not name:
             raise ValueError("Missing Name")
         # if house not in ["gryffindor","hufflepuff","ravenclaw","slytherin"]:
         #     raise ValueError("Invalid House")
         self.name = name
         self.house = house 
-        self.pet = pet
 
 
     def __str__(self):
         return f"{self.name} is from {self.house}"
 
 
-    def charm(self):
-
-        match self.pet:
-            case "stag":
-                return "bbbb"
-            case "otter":
-                return "mmmm"
-            case _:
-                return "......"
-
-
-
 def main():
     student = get_student()
     print(student)
-    print(student.charm())
     # print(f"{student.name} is from {student.house}")
 
 
@@ -38,13 +24,9 @@ def main():
 def get_student():
     name = input("name: ")
     house = input("house: ")
-    pet = input("patronus: ")
-    return Student(name,house,pet) 
-    
-    
+    return Student(name,house)
+
 
 
 if __name__ == "__main__":
     main()
-
-
