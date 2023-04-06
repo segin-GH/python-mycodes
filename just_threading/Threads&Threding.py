e4cae9852e32da4d7bb90f5769bd031e077a1a3a
segin-GH <segin.bytes@gmail.com>
2023-04-06 11:53:56 +0530

    .

--- just_threading/Threads&Threding.py
+++ just_threading/Threads&Threding.py
@@ -2,32 +2,35 @@ import os
 from time import sleep
 from threading import Thread
 
-def box ():
-    while True:
-        print(os.getpid())
-        print("my box is open")
-        sleep(5)
-        print("my box is close")
-        sleep(5)
 
-def led():
-    while True:
-        print(os.getpid())
-        print("my led is on")
-        sleep(2)
-        print("my led is off")
-        sleep(2)
+def main():
+    def box():
+        while True:
+            print(os.getpid())
+            print("my box is open")
+            sleep(5)
+            print("my box is close")
+            sleep(5)
+
+    def led():
+        while True:
+            print(os.getpid())
+            print("my led is on")
+            sleep(2)
+            print("my led is off")
+            sleep(2)
+
+    boxthread = Thread(target=box)
+    ledthread = Thread(target=led)
+    boxthread.daemon = True
+    ledthread.daemon = True
+
+    boxthread.start()
+    ledthread.start()
+
+    while (True):
+        pass
 
 
-boxthread = Thread(target = box)
-ledthread = Thread(target = led)
-boxthread.daemon = True
-ledthread.daemon = True
-
-boxthread.start()
-ledthread.start()
-
-while(True):
-    pass
-
-
+if __name__ == "__main__":
+    main()
